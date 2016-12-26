ed78c6223852e2df7c73b80fe692316a60add127
Stefan <stefan.britting@t-online.de>
2016-12-26 15:52:42 +0800

    add migration for authentication model error occured: instead of user_id:references user:reference

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,3 @@
 class Authentication < ActiveRecord::Base
-  belongs_to :uesr_id
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226072521) do
+ActiveRecord::Schema.define(version: 20161226073522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "token"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
@@ -31,4 +42,5 @@ ActiveRecord::Schema.define(version: 20161226072521) do
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
+  add_foreign_key "authentications", "users"
 end
