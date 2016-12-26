29df7917838b5b1cf7a6f477284f04be6022b7ca
Stefan <stefan.britting@t-online.de>
2016-12-26 15:06:01 +0800

    add self.create_with_auth_and_hash and fb_token method to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  has_many :authentications, :dependent => :destroy
 
-  include Clearance::User
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+     # last_name: auth_hash["last_name"]
+     # email: auth_hash["extra"]["raw_info"]["email"]
+     # check the input auth_hash
+     byebug
+    user = Authentication.create(email: auth_hash["email"] )
+    user.authentications << (authentication)
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+  # end of class
 end
