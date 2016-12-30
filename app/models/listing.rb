85b2c3226d8af4dd4e11412d18bcdbad99e7ed8e
Stefan <stefan.britting@t-online.de>
2016-12-30 18:45:59 +0800

    add table reservation with associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ActiveRecord::Base
   has_many :tags, through: :listing_tags
   has_many :listing_tags, :dependent => :destroy
   has_many :upload_listings, :dependent => :destroy
+  has_many :reservations, :dependent => :destroy
 end
