5d03fbb321491c2e53f650407ea1a42fdb7429d6
Stefan <stefan.britting@t-online.de>
2017-01-03 18:49:55 +0800

    minor change

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,6 +1,9 @@
 class ReservationMailer < ApplicationMailer
-  def booking_email(customer, host, reservation_id)
-    @url  = 'http://example.com/login' # our email account
+  default from: 'notifications@example.com' # from this account all emails will be sent
+  def new_booking_email(customer, host, reservation_id)
+    @customer = customer
+    @host = host
+    @reservation_id = reservation_id
     mail(to: host.email, subject: 'New Reservation for you Property')
   end
 end
