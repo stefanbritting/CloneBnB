b0fd7f4cac59b8529384f2bcdaf437d52cbda759
Stefan <stefan.britting@t-online.de>
2017-01-03 18:45:39 +0800

    add new_booking_email to ReservationMailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,2 +1,6 @@
 class ReservationMailer < ApplicationMailer
+  def booking_email(customer, host, reservation_id)
+    @url  = 'http://example.com/login' # our email account
+    mail(to: host.email, subject: 'New Reservation for you Property')
+  end
 end
