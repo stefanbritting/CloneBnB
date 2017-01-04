d32ab1513c6c37cbddb78075feafa6cd428bfbab
Stefan <stefan.britting@t-online.de>
2017-01-04 14:59:57 +0800

    minor change

--- app/jobs/reservation_job.rb
+++ app/jobs/reservation_job.rb
@@ -3,6 +3,7 @@ class ReservationJob < ActiveJob::Base
 
   def perform(*args)
     # Do something later
-    
+    # calling like ReservationJob.perform will queue this job in the overall
+    # queue and execute it when its turn
   end
 end
