f3ed84f683cfd73da3ef7d477592c8d449387057
Stefan <stefan.britting@t-online.de>
2017-01-10 13:56:47 +0800

    add rspec gem

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+# RSpec.describe Reservation, type: :model do
+#   describe "#check_availability" do
+#     context "When reservations overlap" do
+#       it "returns false" do
+#
+#       end
+#     end
+#   end
+#   # end of test
+# end
